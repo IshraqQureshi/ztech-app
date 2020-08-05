831d3a8fd7e403f5940692db661a642699baaa0c
IshraqQureshi <ishraqqureshi99@gmail.com>
2020-08-06 00:23:33 +0500

    Fingerprint Module

--- apps/appcontrol/employees/views.py
+++ apps/appcontrol/employees/views.py
@@ -3,6 +3,11 @@ from django.conf import settings
 from . import models
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+import time
+import busio
+from digitalio import DigitalInOut, Direction
+import adafruit_fingerprint
+import serial
 
 def index(request):
     if request.session.get('user') is None:
@@ -41,6 +46,85 @@ def add (request):
     return render(request, data['template_folder'] + '/' + data['template_file'], data)
 
 @csrf_exempt
-def ajax_fingerprint(request):
-    response = {}
+def ajax_fingerprint(request):    
+    
+    
+    for i in range(1, 127):                
+        checkfingerprint = models.Employees.objects.filter(fingerprint_1=i).values()
+        if checkfingerprint.exists() == False:
+            fingerprint_id = i
+            break
+            
+    enroll_finger(request, fingerprint_id)
+
+    response = {'fingerprint_id': fingerprint_id}
     return JsonResponse(response)
+
+def enroll_finger(request, location):
+    
+    uart = serial.Serial("/dev/ttyUSB0", baudrate=57600, timeout=1)
+    finger = adafruit_fingerprint.Adafruit_Fingerprint(uart)
+
+    
+    print("Place finger on sensor...", end="", flush=True)    
+
+    while True:
+        i = finger.get_image()
+        if i == adafruit_fingerprint.OK:
+            print("Image taken")
+            break
+        if i == adafruit_fingerprint.NOFINGER:
+            print(".", end="", flush=True)
+        elif i == adafruit_fingerprint.IMAGEFAIL:
+            print("Imaging error")
+            return False
+        else:
+            print("Other error")
+            return False
+
+    print("Templating...", end="", flush=True)
+    i = finger.image_2_tz(1)
+    if i == adafruit_fingerprint.OK:
+        print("Templated")
+    else:
+        if i == adafruit_fingerprint.IMAGEMESS:
+            print("Image too messy")
+        elif i == adafruit_fingerprint.FEATUREFAIL:
+            print("Could not identify features")
+        elif i == adafruit_fingerprint.INVALIDIMAGE:
+            print("Image invalid")
+        else:
+            print("Other error")
+        return False
+
+    
+    print("Remove finger")
+    time.sleep(1)
+    while i != adafruit_fingerprint.NOFINGER:
+        i = finger.get_image()
+
+    print("Creating model...", end="", flush=True)
+    i = finger.create_model()
+    if i == adafruit_fingerprint.OK:
+        print("Created")
+    else:
+        if i == adafruit_fingerprint.ENROLLMISMATCH:
+            print("Prints did not match")
+        else:
+            print("Other error")
+        return False
+
+    print("Storing model #%d..." % location, end="", flush=True)
+    i = finger.store_model(location)
+    if i == adafruit_fingerprint.OK:
+        print("Stored")
+    else:
+        if i == adafruit_fingerprint.BADLOCATION:
+            print("Bad storage location")
+        elif i == adafruit_fingerprint.FLASHERR:
+            print("Flash storage error")
+        else:
+            print("Other error")
+        return False
+
+    return True
--- static/appcontrol/js/ajax.js
+++ static/appcontrol/js/ajax.js
@@ -2,6 +2,8 @@ $(document).ready(function(){
 
     $('#fingerprint_btn_1').click(function(e){
         e.preventDefault();
+        $('.finger_print_popup').addClass('active');
+        $('input[name=fingerprint_1]').val('')
         register_fingerprint()
     });
 
@@ -9,12 +11,25 @@ $(document).ready(function(){
 
 function register_fingerprint(fingerprint_type=false)
 {
-    
+    $('.fingerprint_success').hide();   
     $.ajax({
         url: '/appcontrol/employees/ajax_register_fingerprint/',
         type: 'POST',        
         success: function(response){
-            console.log(response)
+            
+            if ( response != '' )
+            {
+                $('.finger_print_popup').removeClass('active');
+
+                $('.finger_print_success').addClass('active');
+                setTimeout(() => {
+                    $('.finger_print_success').removeClass('active');    
+                }, 3000);
+                // response = JSON.parse(response);
+                $('input[name=fingerprint_1]').val(response.fingerprint_id)
+                $('.fingerprint_success').show();
+            }
+            
         }
     })
 }
